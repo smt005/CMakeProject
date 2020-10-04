63b4328b0c7e87681816524d194cfd8eb3bc349e
Коптяев Александр <a.koptyaev@playrix.com>
2020-10-05 01:19:54 +0300

    Подключен Physx CMakeProject\Source\Engine\ThirdParty\PhysX\physx\externals\physx\bin\win.x86_32.vc141.md\debug\PhysXCooking_static.lib CMakeProject\Source\Engine\ThirdParty\PhysX\physx\externals\physx\bin\win.x86_32.vc141.md\debug\PhysXExtensions_static.lib

--- Script/Windows_vs17_x64.cmd
+++ Script/Windows_vs17_x64.cmd
@@ -1,4 +1,4 @@
 @echo off
-mkdir "../build/Win"
-cd "../build/Win"
+mkdir "../build/Win_x64"
+cd "../build/Win_x64"
 cmake -G "Visual Studio 15 2017 Win64" ../../Source
--- Script/Windows_vs19_x64.cmd
+++ Script/Windows_vs19_x64.cmd
@@ -1,4 +1,4 @@
 @echo off
-mkdir "../build/Win"
-cd "../build/Win"
+mkdir "../build/Win_x64"
+cd "../build/Win_x64"
 cmake -G "Visual Studio 16 2019 Win64" ../../Source
